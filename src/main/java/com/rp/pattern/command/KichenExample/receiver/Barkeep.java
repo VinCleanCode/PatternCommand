ecfb4dcfa2d4a8b4a2cc52ee14407fd720666bfa
vincentmylee <vincentmylee@gmail.com>
2019-10-16 08:06:34 +0800

    Finish KitchenExample

--- src/main/java/com/rp/pattern/command/KichenExample/receiver/Barkeep.java
+++ src/main/java/com/rp/pattern/command/KichenExample/receiver/Barkeep.java
@@ -1,6 +1,6 @@
 package com.rp.pattern.command.KichenExample.receiver;
 
-public class Brkeep implements KitchenWorker {
+public class Barkeep implements KitchenWorker {
     public void finishOrder() {
         System.out.println("Glass->Ice->Drink->Done\n");
     }
--- src/test/java/com/rp/pattern/command/KitchenExample/CoffeShopClient.java
+++ src/test/java/com/rp/pattern/command/KitchenExample/CoffeShopClient.java
@@ -1,9 +1,10 @@
 package com.rp.pattern.command.KitchenExample;
 
+import com.rp.pattern.command.KichenExample.Invoker.Waitress;
 import com.rp.pattern.command.KichenExample.command.DrinkerOrder;
 import com.rp.pattern.command.KichenExample.command.Order;
 import com.rp.pattern.command.KichenExample.command.SnackOrder;
-import com.rp.pattern.command.KichenExample.receiver.Brkeep;
+import com.rp.pattern.command.KichenExample.receiver.Barkeep;
 import com.rp.pattern.command.KichenExample.receiver.Chef;
 
 public class CoffeShopClient {
@@ -11,8 +12,27 @@ public class CoffeShopClient {
 		System.out.println("======================Command Pattern TEST===================");
 
 		Chef snackChef = new Chef();
-		Brkeep brkeep = new Brkeep();
+		Barkeep barkeep = new Barkeep();
 		Order snackOrder = new SnackOrder(snackChef);
-		Order drinkOrder = new DrinkerOrder(brkeep);
+		Order drinkOrder = new DrinkerOrder(barkeep);
+
+		Waitress w1 = new Waitress();
+		System.out.println("=======Customer order=======");
+		w1.setOrder(snackOrder);
+		w1.setOrder(snackOrder);
+		w1.setOrder(drinkOrder);
+		w1.setOrder(drinkOrder);
+
+		w1.setOrder(drinkOrder);
+		System.out.println("=============Customer cancel snack==============");
+		w1.cancelOrder(snackOrder);
+
+		w1.setOrder(snackOrder);
+		System.out.println("====== Finished order, notify Kitchen =========");
+
+		w1.notifyBaker();
+		System.out.println();
+		System.out.println("===== snack stock not enough ======");
+		w1.setOrder(snackOrder);
 	}
 }
